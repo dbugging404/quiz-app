64bffa03f92689456bed27fb506c69508c20c4b0
dbugging404 <theavinashmishra13@gmail.com>
2024-06-24 15:41:35 +0530

    added new options.
    - add upto 2 images in questions.
    - add multiple correct answers to a question.
    - have upto 5 options.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,25 @@
-import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 interface Question {
   question: string;
-  image: string;
-  image2: string;
+  image?: string;
+  image2?: string;
   options: {
     a: string;
     b: string;
     c: string;
     d: string;
+    e?: string; // Make e optional
   };
-  answer: 'a' | 'b' | 'c' | 'd';
+  answer: ('a' | 'b' | 'c' | 'd' | 'e')[];
 }
 
 const App: React.FC = () => {
   const [questions, setQuestions] = useState<Question[]>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
-  const [selectedOptions, setSelectedOptions] = useState<(string | null)[]>([]);
+  const [selectedOptions, setSelectedOptions] = useState<string[][]>([]);
   const [isQuizFinished, setIsQuizFinished] = useState(false);
   const [mode, setMode] = useState<
     'normal' | 'show-answer' | 'instant-feedback'
@@ -26,6 +27,7 @@ const App: React.FC = () => {
   const [feedback, setFeedback] = useState<string | null>(null);
 
   useEffect(() => {
+    // Fetch quiz data from JSON
     fetch('/quizData.json')
       .then((response) => response.json())
       .then((data) => {
@@ -38,12 +40,13 @@ const App: React.FC = () => {
           setSelectedOptions(selectedOptions);
           setScore(score);
         } else {
-          setSelectedOptions(Array(data.length).fill(null));
+          setSelectedOptions(Array(data.length).fill([]));
         }
       });
   }, []);
 
   useEffect(() => {
+    // Save quiz state to local storage
     if (questions.length > 0) {
       const quizState = {
         currentQuestionIndex,
@@ -55,17 +58,46 @@ const App: React.FC = () => {
   }, [currentQuestionIndex, selectedOptions, score, questions]);
 
   const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const option = e.target.value as 'a' | 'b' | 'c' | 'd' | 'e';
+    const isSelected = e.target.checked;
     const newSelectedOptions = [...selectedOptions];
-    newSelectedOptions[currentQuestionIndex] = e.target.value;
+    if (!newSelectedOptions[currentQuestionIndex]) {
+      newSelectedOptions[currentQuestionIndex] = [];
+    }
+    const currentSelections = [...newSelectedOptions[currentQuestionIndex]];
+    if (isSelected) {
+      currentSelections.push(option);
+    } else {
+      const index = currentSelections.indexOf(option);
+      if (index > -1) {
+        currentSelections.splice(index, 1);
+      }
+    }
+
+    newSelectedOptions[currentQuestionIndex] = currentSelections;
     setSelectedOptions(newSelectedOptions);
 
     if (mode === 'instant-feedback') {
-      handleCheckAnswer(e.target.value);
+      handleCheckAnswer(currentSelections);
     }
   };
 
-  const handleCheckAnswer = (selectedOption: string) => {
-    if (selectedOption === questions[currentQuestionIndex].answer) {
+  const handleCheckAnswer = (selectedOption: string[] | null) => {
+    const correctAnswers = questions[currentQuestionIndex].answer;
+
+    // Ensure correctAnswers is always treated as an array
+    const answersArray = Array.isArray(correctAnswers)
+      ? correctAnswers
+      : [correctAnswers];
+
+    // Check if selectedOption is not null or undefined and has the same length as correctAnswers
+    if (
+      selectedOption &&
+      selectedOption.length === answersArray.length &&
+      selectedOption.every((option) =>
+        answersArray.includes(option as 'a' | 'b' | 'c' | 'd' | 'e')
+      )
+    ) {
       setFeedback('Correct!');
       setScore(score + 1);
     } else {
@@ -75,13 +107,20 @@ const App: React.FC = () => {
 
   const handleNextQuestion = () => {
     const selectedOption = selectedOptions[currentQuestionIndex];
+    const correctAnswers = questions[currentQuestionIndex].answer;
+
     if (
-      selectedOption !== null &&
-      selectedOption === questions[currentQuestionIndex].answer
+      selectedOption &&
+      selectedOption.length === correctAnswers.length &&
+      selectedOption.every((option) =>
+        correctAnswers.includes(option as 'a' | 'b' | 'c' | 'd' | 'e')
+      )
     ) {
       setScore(score + 1);
     }
+
     setFeedback(null);
+
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
@@ -109,7 +148,7 @@ const App: React.FC = () => {
   const handleResetQuiz = () => {
     if (window.confirm('Are you sure you want to reset the quiz?')) {
       setCurrentQuestionIndex(0);
-      setSelectedOptions(Array(questions.length).fill(null));
+      setSelectedOptions(Array(questions.length).fill([]));
       setScore(0);
       setIsQuizFinished(false);
       setMode('normal');
@@ -117,6 +156,7 @@ const App: React.FC = () => {
       localStorage.removeItem('quizState');
     }
   };
+
   if (questions.length === 0) {
     return <div>Loading...</div>;
   }
@@ -129,8 +169,8 @@ const App: React.FC = () => {
     );
   }
 
+  // Ensure currentQuestion is defined before accessing its properties
   const currentQuestion = questions[currentQuestionIndex];
-  const correctAnswer = questions[currentQuestionIndex].answer;
 
   return (
     <div className='flex items-center justify-start mx-auto font-body px-2'>
@@ -160,7 +200,6 @@ const App: React.FC = () => {
             >
               Normal Mode
             </button>
-
             <button
               onClick={() => toggleMode('instant-feedback')}
               className={`max-w-fit px-3 py-1 rounded-full lg:my-6 ${
@@ -183,10 +222,14 @@ const App: React.FC = () => {
         </div>
         <div className='flex flex-col justify-center max-w-5xl mx-auto min-h-[80vh] border-b pb-6'>
           <div className='text-xl py-8'>
-            {currentQuestionIndex + 1}. {currentQuestion.question}
+            {currentQuestion && (
+              <>
+                {currentQuestionIndex + 1}. {currentQuestion.question}
+              </>
+            )}
           </div>
           <div>
-            {currentQuestion.image && (
+            {currentQuestion && currentQuestion.image && (
               <Image
                 src={currentQuestion.image}
                 alt='quiz'
@@ -195,9 +238,9 @@ const App: React.FC = () => {
                 height={300}
               />
             )}
-            {currentQuestion.image2 && (
+            {currentQuestion && currentQuestion.image2 && (
               <Image
-                src={currentQuestion.image2 && currentQuestion.image2}
+                src={currentQuestion.image2}
                 alt='quiz2'
                 className=' mx-auto mb-4'
                 width={450}
@@ -209,32 +252,31 @@ const App: React.FC = () => {
             Options
           </div>
           <div className='flex flex-col space-y-4'>
-            {Object.keys(currentQuestion.options).map((key) => (
-              <label key={key} className='text-base '>
-                <input
-                  type='radio'
-                  name='option'
-                  value={key}
-                  className='mr-3'
-                  checked={selectedOptions[currentQuestionIndex] === key}
-                  onChange={handleOptionChange}
-                  disabled={mode === 'show-answer'}
-                />
-                {
-                  currentQuestion.options[
-                    key as keyof typeof currentQuestion.options
-                  ]
-                }
-              </label>
-            ))}
-            {mode === 'show-answer' && (
+            {currentQuestion &&
+              Object.keys(currentQuestion.options).map((key) => (
+                <label key={key} className='text-base'>
+                  <input
+                    type='checkbox'
+                    name='option'
+                    value={key}
+                    className='mr-3'
+                    checked={(
+                      selectedOptions[currentQuestionIndex] || []
+                    ).includes(key as 'a' | 'b' | 'c' | 'd' | 'e')}
+                    onChange={handleOptionChange}
+                    disabled={mode === 'show-answer'}
+                  />
+                  {
+                    currentQuestion.options[
+                      key as keyof typeof currentQuestion.options
+                    ]
+                  }
+                </label>
+              ))}
+            {mode === 'show-answer' && currentQuestion && (
               <div className='bg-green-300 px-3 py-2 rounded-lg'>
                 <span className='font-bold text-lg'>Correct Answer: </span>
-                {
-                  currentQuestion.options[
-                    correctAnswer as keyof typeof currentQuestion.options
-                  ]
-                }
+                {currentQuestion.options[currentQuestion.answer[0]]}
               </div>
             )}
             {mode === 'instant-feedback' && feedback && <div>{feedback}</div>}
@@ -262,7 +304,9 @@ const App: React.FC = () => {
                 key={index}
                 onClick={() => handleSkipToQuestion(index)}
                 className={`w-10 h-10 rounded-full hover:bg-blue-500 ${
-                  selectedOptions[index] ? 'bg-yellow-500' : 'bg-blue-200'
+                  selectedOptions[index]?.length > 0
+                    ? 'bg-yellow-500'
+                    : 'bg-blue-200'
                 } ${
                   currentQuestionIndex === index
                     ? 'bg-blue-500 text-white'
